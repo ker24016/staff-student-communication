a521cdbaa04cef7c640471709253e6a7bb0a9025
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-03-14 15:24:59 -0600

    Back to basics - H2 Database

--- src/main/java/com/byui/studentstaffcommunication/StudentStaffCommunicationApplication.java
+++ src/main/java/com/byui/studentstaffcommunication/StudentStaffCommunicationApplication.java
@@ -1,42 +1,13 @@
 package com.byui.studentstaffcommunication;
 
-import com.byui.studentstaffcommunication.model.User;
-import com.byui.studentstaffcommunication.model.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class StudentStaffCommunicationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentStaffCommunicationApplication.class, args);
-    }
-
-    @Component
-    public static class DatabaseSeeder implements CommandLineRunner {
-
-        private final UserRepository userRepository;
-
-        public DatabaseSeeder(UserRepository userRepository) {
-            this.userRepository = userRepository;
-        }
-
-        @Override
-        public void run(String... args) {
-            if (userRepository.count() == 0) { // Prevent duplicate seeding
-                User user1 = new User();
-                user1.setEmail("user1@example.com");
-                user1.setUsername("user1");
-
-                userRepository.save(user1);
-
-                System.out.println("Database seeded!");
-            } else {
-                System.out.println("Database already seeded.");
-            }
-        }
     }
 
 }
