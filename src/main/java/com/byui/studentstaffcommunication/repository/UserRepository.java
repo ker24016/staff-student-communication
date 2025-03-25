df443da6435b9e6431789373a8fb92583a3fe899
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-03-25 14:38:24 -0600

    Add missing Repository annotation

--- src/main/java/com/byui/studentstaffcommunication/repository/UserRepository.java
+++ src/main/java/com/byui/studentstaffcommunication/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.byui.studentstaffcommunication.repository;
 import com.byui.studentstaffcommunication.model.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> getAllByNameContainingIgnoreCase(String name, Pageable pageable);
 
