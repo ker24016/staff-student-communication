764d4082fdfe0b0629f9a03e4d6ff2f582443cd5
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-03-27 14:31:11 -0600

    Optimize imports

--- src/main/java/com/byui/studentstaffcommunication/WebSecurityConfig.java
+++ src/main/java/com/byui/studentstaffcommunication/WebSecurityConfig.java
@@ -1,16 +1,12 @@
 package com.byui.studentstaffcommunication;
-import com.byui.studentstaffcommunication.service.CustomUserDetailsService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
--- src/main/java/com/byui/studentstaffcommunication/model/User.java
+++ src/main/java/com/byui/studentstaffcommunication/model/User.java
@@ -1,7 +1,6 @@
 package com.byui.studentstaffcommunication.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
